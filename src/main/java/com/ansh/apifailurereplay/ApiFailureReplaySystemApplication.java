f8f8edc57e9d6d0daa279ae3c213053d4adcca40
ansh raj <anshrajsingh6565555@gmail.com>
2026-05-29 17:54:34 +0530

    adding and setup files structures and properties

---
+++ src/main/java/com/ansh/apifailurereplay/ApiFailureReplaySystemApplication.java
@@ -0,0 +1,13 @@
+package com.ansh.apifailurereplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFailureReplaySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFailureReplaySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ansh/apifailurereplay/ApiFailureReplaySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansh.apifailurereplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFailureReplaySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
